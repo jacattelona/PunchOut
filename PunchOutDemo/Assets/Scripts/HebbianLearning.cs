a7c31749564a212fd94f48bffab3c66aefecd458
Kyle Corry <kylecorry31@gmail.com>
2019-09-08 07:24:36 -0400

    Add hebbian learning infra

---
+++ PunchOutDemo/Assets/Scripts/HebbianLearning.cs
@@ -0,0 +1,121 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HebbianLearning : Decision
+{
+    public int numInput = 29;
+    public int numOutput = 4;
+    public int numHidden = 32;
+    public int hiddenLayers = 2;
+
+    private Neuron[][] neurons;
+    private List<Synapse> synapses;
+
+    void Start()
+    {
+        synapses = new List<Synapse>();
+        neurons = new Neuron[2 + hiddenLayers][];
+        neurons[0] = new Neuron[numInput];
+        neurons[1 + hiddenLayers] = new Neuron[numOutput];
+        for (var i = 1; i < 1 + hiddenLayers; i++)
+        {
+            neurons[i] = new Neuron[numHidden];
+        }
+
+        for (var i = 0; i < neurons.Length; i++)
+        {
+            for (var j = 0; j < neurons[i].Length; j++)
+            {
+                neurons[i][j] = new Neuron();
+            }
+        }
+
+        for (var i = 0; i < neurons.Length - 1; i++)
+        {
+            for (var j = 0; j < neurons[i].Length; j++)
+            {
+                for (var k = 0; k < neurons[i + 1].Length; k++)
+                {
+                    var synapse = neurons[i][j].AddOutgoingConnection(neurons[i + 1][k]);
+                    synapses.Add(synapse);
+                }
+            }
+        }
+
+    }
+
+    public override float[] Decide(List<float> vectorObs, List<Texture2D> visualObs, float reward, bool done, List<float> memory)
+    {
+
+        if (reward < 0) // Flood brain with inhibitors
+        {
+            foreach (Synapse synapse in synapses)
+            {
+                synapse.SetConnectionType(Synapse.Type.INHIBITOR);
+            }
+        }
+        else if (reward >= 0) // Flood brain with stimulants
+        {
+            foreach (Synapse synapse in synapses)
+            {
+                synapse.SetConnectionType(Synapse.Type.STIMULANT);
+            }
+        }
+       
+        for (var i = 0; i < vectorObs.Count; i++)
+        {
+            neurons[0][i].StimulateChannels(vectorObs[i]);
+            neurons[0][i].PullInIons();
+        }
+
+        for (var i = 1; i < neurons.Length - 1; i++)
+        {
+            foreach (Neuron n in neurons[i])
+            {
+                n.PullInIons();
+            }
+        }
+
+        bool[] output = new bool[numOutput];
+        for (var i = 0; i < neurons[neurons.Length - 1].Length; i++)
+        {
+            output[i] = neurons[neurons.Length - 1][i].PullInIons();
+        }
+
+        float punchState = 0f;
+        float dodgeState = 0f;
+
+        if (output[0] && !output[1])
+        {
+            punchState = 1f;
+        }
+        else if (!output[0] && output[1])
+        {
+            punchState = 2f;
+        }
+
+        if (output[2] && !output[3])
+        {
+            dodgeState = 1f;
+        }
+        else if (!output[2] && output[3])
+        {
+            dodgeState = 2f;
+        }
+
+
+
+        return new float[] { dodgeState, punchState };
+    }
+
+
+    public override List<float> MakeMemory(List<float> vectorObs, List<Texture2D> visualObs, float reward, bool done, List<float> memory)
+    {
+        // DO nothing
+        return memory;
+    }
+
+
+}
---
+++ PunchOutDemo/Assets/Scripts/Synapse.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Synapse
+{
+    public enum Type
+    {
+        STIMULANT,
+        INHIBITOR
+    }
+
+    private Neuron from, to;
+    private float weight;
+    private Type type;
+    private bool fired;
+
+    public Synapse(Neuron from, float weight)
+    {
+        this.from = from;
+        this.weight = weight;
+        this.type = Type.STIMULANT;
+        this.to = null;
+    }
+
+
+    public Neuron GetFrom()
+    {
+        return from;
+    }
+
+    public void SetFrom(Neuron from)
+    {
+        this.from = from;
+    }
+
+    public Neuron GetTo()
+    {
+        return to;
+    }
+
+    public void SetTo(Neuron to)
+    {
+        this.to = to;
+    }
+
+    public float GetWeight()
+    {
+        return weight;
+    }
+
+    public void SetWeight(float weight)
+    {
+        this.weight = weight;
+    }
+
+    public Type GetConnectionType()
+    {
+        return type;
+    }
+
+    public void SetConnectionType(Type type)
+    {
+        this.type = type;
+    }
+
+    public void NoFire()
+    {
+        fired = false;
+    }
+
+    public void Fire()
+    {
+        if (to == null)
+        {
+            return;
+        }
+
+        switch (type)
+        {
+            case Type.STIMULANT:
+                to.StimulateChannels(Mathf.Max(0, weight));
+                break;
+            case Type.INHIBITOR:
+                to.InhibitChannels(Mathf.Max(0, weight));
+                break;
+        }
+
+        //        to.pullInIons();
+
+        fired = true;
+    }
+
+    public void ToFired(bool toFired)
+    {
+        if (fired && toFired)
+        {
+            weight += 0.001f;
+        }
+        else if (toFired)
+        {
+            weight -= 0.0001f;
+        }
+        else if (fired)
+        {
+            weight -= 0.00001f;
+        }
+        else
+        {
+            // Nothing
+        }
+    }
+
+}
