70f91dce73fdc0777157bd43a02b8819d5c08f17
Grant Ferguson <gferguson@wpi.edu>
2019-09-11 16:06:32 -0400

    Demo Thingy

---
+++ PunchOutDemo/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nextLevel;
+    public Button a;
+    
+    public GameObject spirite1;
+    public GameObject spirite2;
+    public GameObject spirite3;
+    void Start()
+    {
+        a.onClick.AddListener(checkLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spirite1.activeSelf && spirite1.activeSelf && spirite1.activeSelf)
+        {
+            
+        }
+    }
+
+    void checkLevel()
+    {
+        if (spirite1.activeSelf && spirite1.activeSelf && spirite1.activeSelf)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
