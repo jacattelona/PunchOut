8771c9ba90e8d4777326017eec3ad54493d71d82
Grant Ferguson <gferguson@wpi.edu>
2019-09-11 14:37:02 -0400

    Scene Changer

---
+++ PunchOutDemo/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Change(nextLevel);
+        }
+    }
+    
+    public void Change(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
