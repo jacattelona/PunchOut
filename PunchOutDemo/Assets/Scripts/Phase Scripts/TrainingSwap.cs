863ae4b65e7deede63e7730b6293c7be2bef761d
Unknown <jacattelona@wpi.edu>
2019-09-13 10:21:45 -0400

    Phase Switching Script for Training

---
+++ PunchOutDemo/Assets/Scripts/Phase Scripts/TrainingSwap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingSwap : MonoBehaviour
+{
+    public Vector3 offenseLocation;
+    public Vector3 defenseLocation;
+
+    bool isSwapping = false;
+
+    Vector3 target;
+    Vector3 start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSwapping)
+        {
+
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+
+            }
+        }
+
+    }
+}
