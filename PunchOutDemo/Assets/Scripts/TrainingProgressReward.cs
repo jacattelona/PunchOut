d455fe84a905f2eb2bbaea80b0733710985553fa
Kyle Corry <kylecorry31@gmail.com>
2019-09-24 14:55:35 -0400

    Add training progress UI

---
+++ PunchOutDemo/Assets/Scripts/TrainingProgressReward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingProgressReward : MonoBehaviour
+{
+    [SerializeField]
+    private TrainingProgress trainingProgress;
+
+    [SerializeField]
+    private RewardHistory rewardHistory;
+
+    private void Update()
+    {
+        if (rewardHistory.rewards.Count == 0) return;
+        trainingProgress.value = rewardHistory.rewards[rewardHistory.rewards.Count - 1].Amount;
+    }
+}
