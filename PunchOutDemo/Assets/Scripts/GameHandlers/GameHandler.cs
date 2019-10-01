35b9157ae70dca9cba95dcfdf56f52ed7e224878
Unknown <jacattelona@wpi.edu>
2019-10-01 09:46:06 -0400

    S̴̨̳̹̠̪͇̼̱̺̠̺̍̒̓̒͛̓̍̑͌̀̉͂̉͛̓ͅữ̴̟̙̘̥̙͍͇͙̭̼̼̲̬͋͋̑͗͌͑̄͜f̵̧̯͂f̴̜͇̙͚͇̬̑́̓̈́̆̽̀̈̉̏̚͝ͅe̵̛̥̩͔̮̜̝͔̙̪͉̯̊̿̿̏͋̌̾̊͘͘͝ṟ̴̛͈̲͚̆͐̀̓͊̓̇̓̽̔͝i̵̡͖̦̥̦̮̖̝̹̮̼͋̎͊̿͜ņ̸̖͖͙͉͓̩͔̖̂ǧ̸̤̻̮̜͗̇̈̕͝͝͠
    Phase swapping in ML

--- PunchOutDemo/Assets/Scripts/GameHandlers/GameHandler.cs
+++ PunchOutDemo/Assets/Scripts/GameHandlers/GameHandler.cs
@@ -1,22 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHandler : MonoBehaviour
 {
     public Transform cameraTransform;
     private Vector3 cameraVelocity = Vector3.zero;
+    public Vector3 offenseLocation, defenseLocation, matchLocation;
 
     [SerializeField]
     public GameObject mainMenu, offensiveTraining, defensiveTraining, match;
+    public Text timer;
 
-    private const int STATE_MAIN_MENU = 0, STATE_OFFENSIVE = 1, STATE_DEFENSIVE = 3, STATE_MATCH = 4;
+    private const int STATE_MAIN_MENU = 0, STATE_OFFENSIVE = 1, STATE_DEFENSIVE = 3, STATE_MATCH = 4, STATE_TO_DEFENSIVE = 5, STATE_TO_MATCH = 6;
 
     private int state = STATE_MAIN_MENU;
+
+    float timeLeft = 0;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        state = STATE_MAIN_MENU;
     }
 
     // Update is called once per frame
@@ -26,12 +32,46 @@ public class GameHandler : MonoBehaviour
         switch (state)
         {
             case STATE_MAIN_MENU:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StartOffensive();
+                }
                 break;
+
             case STATE_OFFENSIVE:
+                TimeUpdate();
+                if (timeLeft <= 0)
+                    SwitchDefensive();
                 break;
+
             case STATE_DEFENSIVE:
+                TimeUpdate();
+                if (timeLeft <= 0)
+                    SwitchMatch();
                 break;
+
             case STATE_MATCH:
+                break;
+
+            case STATE_TO_DEFENSIVE:
+                if (MoveCameraToPosition(defenseLocation))
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        StartDefensive();
+                    }
+                    //StartDefensive();
+                }
+                break;
+            case STATE_TO_MATCH:
+                if (MoveCameraToPosition(matchLocation))
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        StartMatch();
+                    }
+                }
+                    
                 break;
         }
     }
@@ -40,6 +80,66 @@ public class GameHandler : MonoBehaviour
     {
         cameraTransform.position = Vector3.SmoothDamp(cameraTransform.position, position, ref cameraVelocity, .5f);
         return Vector3.Distance(cameraTransform.position, position) < .01f;
+    }
+
+    private void StartOffensive()
+    {
+        state = STATE_OFFENSIVE;
+        timeLeft = 60f;
+        timer.text = "Time Left: " + (int)timeLeft;
+
+        offensiveTraining.SetActive(true);
+        defensiveTraining.SetActive(false);
+        match.SetActive(false);
+    }
+
+    //private void OffensiveUpdate()
+    //{
+    //    TimeUpdate();
+    //}
+
+    
+    private void SwitchDefensive()
+    {
+        state = STATE_TO_DEFENSIVE;
+        timeLeft = 60f;
+        timer.text = "Press Space to Start";
+
+        offensiveTraining.SetActive(false);
+        defensiveTraining.SetActive(true);
+        match.SetActive(false);
+    }
+
+    private void StartDefensive()
+    {
+        state = STATE_DEFENSIVE;
+        timer.text = "Time Left: " + (int)timeLeft;
+    }
+
+
+    private void SwitchMatch()
+    {
+        state = STATE_TO_MATCH;
+        timeLeft = 60f;
+        timer.text = "Press Space to Start";
+
+        offensiveTraining.SetActive(false);
+        defensiveTraining.SetActive(false);
+        match.SetActive(true);
+        match.GetComponent<Match>().StopFight();
+    }
+
+    private void StartMatch()
+    {
+        state = STATE_MATCH;
+        timer.text = "Fight!";
+        match.GetComponent<Match>().StartFight();
+    }
+
+    private void TimeUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = "Time Left: " + (int)timeLeft;
     }
 
 }
