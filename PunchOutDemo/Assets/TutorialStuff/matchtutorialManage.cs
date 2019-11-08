7fb896e94b3e506b015c153b1b973f62c5a793ed
Grant Ferguson <gferguson@wpi.edu>
2019-11-08 10:02:42 -0500

    Icons and buttons

--- PunchOutDemo/Assets/TutorialStuff/matchtutorialManage.cs
+++ PunchOutDemo/Assets/TutorialStuff/matchtutorialManage.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class matchtutorialManage : MonoBehaviour
 {
     public GameHandler handle;
     public Animator anim;
+    public Button fight;
+    public Button train;
+
     // Start is called before the first frame update
     void Start()
     {
         anim.SetTrigger("trigger1");
+        fight.onClick.AddListener(goFight);
+        train.onClick.AddListener(goTrain);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown("t"))
         {
             handle.StartOffensive();
@@ -24,4 +31,16 @@ public class matchtutorialManage : MonoBehaviour
             handle.setState(6);
         }
     }
+
+    public void goTrain()
+    {
+        handle.StartOffensive();
+    }
+
+    public void goFight()
+    {
+        handle.setState(6);
+    }
+
+    
 }
